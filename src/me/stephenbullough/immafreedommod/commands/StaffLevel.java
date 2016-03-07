9b3e6477fb991797110b5ee49cba8144216fb741
OutOfYourMind <outofyourmindyt@gmail.com>
2016-03-07 14:07:23 -0500

    Update StaffLevel.java

--- src/me/stephenbullough/immafreedommod/commands/StaffLevel.java
+++ src/me/stephenbullough/immafreedommod/commands/StaffLevel.java
@@ -2,7 +2,7 @@ package me.stephenbullough.immafreedommod.commands;
 
 public enum StaffLevel
 {
-    ALL("All Player Commands"), OP("OP Commands"), STAFF1("SuperAdmin Commands"), STAFF2("Senior Admin Commands");
+    ALL("All Player Commands"), DEFAULT("DEFAULT Commands"), HELPER("HELPER Commands"), MOD("MOD Commands"), ADMIN("ADMIN Commands"), EXEC("EXECUTIVE Commands");
     //
     private final String friendlyName;
 
