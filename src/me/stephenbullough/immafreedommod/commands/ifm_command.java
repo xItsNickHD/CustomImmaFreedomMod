0376dd398a477be111573722ad5852956f0adef7
๖ۣۜItsNickx <itsnick.roberts@gmail.com>
2016-03-09 18:50:57 +1100

    Update ifm_command.java

--- src/me/stephenbullough/immafreedommod/commands/ifm_command.java
+++ src/me/stephenbullough/immafreedommod/commands/ifm_command.java
@@ -12,7 +12,8 @@ public abstract class ifm_command
     public static final String MSG_NO_PERMS = ChatColor.YELLOW + "You do not have permission to use this command.";
     public static final String YOU_ARE_OP = ChatColor.YELLOW + "You are now op!";
     public static final String YOU_ARE_NOT_OP = ChatColor.YELLOW + "You are no longer op!";
-    public static final String NOT_FROM_CONSOLE = "This command may not be used from the console.";
+    public static final String NOT_FROM_CONSOLE = "This command can only be executed through the console!"
+    public static final String NOT_FROM_CONSOLE = "This command may not be used from the console!";
     public static final String PLAYER_NOT_FOUND = ChatColor.GRAY + "Player not found!";
     protected immafreedommod plugin;
     protected Server server;
